fad46c6b218be4002a70d22180e4169cd51fd675
akhi9550 <akhilc248@gmail.com>
2024-04-17 16:20:26 +0530

    Api-Gateway

--- GO-GRPC-API-GATEWAY/pkg/utils/models/chat.go
+++ GO-GRPC-API-GATEWAY/pkg/utils/models/chat.go
@@ -6,22 +6,6 @@ import (
 	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
-type Chat struct {
-	ID              primitive.ObjectID `json:"id" bson:"_id,omitempty"`
-	Users           []uint             `json:"users" bson:"users"`
-	LastMessage     string             `json:"last_message" bson:"last_message"`
-	LastMessageTime time.Time          `json:"last_message_time" bson:"last_message_time"`
-}
-
-// type Message struct {
-// 	Message string `json:"message"`
-// }
-
-type ChatResponse struct {
-	Chat Chat
-	User UserData
-}
-
 type Messages struct {
 	ID             primitive.ObjectID `json:"id" bson:"_id,omitempty"`
 	SenderID       uint               `json:"sender_id" bson:"sender_id"`
