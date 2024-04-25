c4d85d94e323a3ead1191ef68e25073a1002d206
Akhil C <141711034+akhi9550@users.noreply.github.com>
2024-04-25 12:32:08 +0530

    Delete GO-GRPC--API-GATEWAY directory

--- GO-GRPC--API-GATEWAY/pkg/client/interface/notification.go
+++
@@ -1,9 +0,0 @@
-package interfaces
-
-// import "github.com/akhi9550/api-gateway/pkg/utils/models"
-
-// type NotificationClient interface {
-// 	SendLikeNotification(userID, postID int) (string, error)
-// 	ConsumeKafkaLikeMessages(userID int) (models.Responses,error)
-// 	ConsumeKafkaCommentMessages(userID int)(models.Responses,error)
-// }
