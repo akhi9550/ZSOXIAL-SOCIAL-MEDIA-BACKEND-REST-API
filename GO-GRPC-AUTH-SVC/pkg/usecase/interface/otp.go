c18972542c5d0a4c7745a02c72e439eed918aefa
akhi9550 <akhilc248@gmail.com>
2024-05-17 20:27:45 +0530

    Auth_svc

--- GO-GRPC-AUTH-SVC/pkg/usecase/interface/otp.go
+++
@@ -1,9 +0,0 @@
-
-package interfaces
-
-import "github.com/akhi9550/auth-svc/pkg/utils/models"
-
-type OtpUseCase interface {
-	SendOtp(phone string) error
-	VerifyOTP(code models.VerifyData) (models.ReponseWithToken, error)
-}
