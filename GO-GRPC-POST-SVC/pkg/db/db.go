8ca42d1b3890cd4a52cec3b4859b13c34c75b609
akhi9550 <akhilc248@gmail.com>
2024-06-20 13:57:07 +0530

    Automatically Created Database

--- GO-GRPC-POST-SVC/pkg/db/db.go
+++ GO-GRPC-POST-SVC/pkg/db/db.go
@@ -10,10 +10,46 @@ import (
 )
 
 func ConnectDatabase(cfg config.Config) (*gorm.DB, error) {
-	psqlInfo := fmt.Sprintf("host=%s user=%s dbname=%s port=%s password=%s", cfg.DBHost, cfg.DBUser, cfg.DBName, cfg.DBPort, cfg.DBPassword)
-	db, dbErr := gorm.Open(postgres.Open(psqlInfo), &gorm.Config{
+	psqlInfo := fmt.Sprintf("host=%s user=%s dbname=postgres port=%s password=%s sslmode=disable",
+		cfg.DBHost, cfg.DBUser, cfg.DBPort, cfg.DBPassword)
+
+	db, err := gorm.Open(postgres.Open(psqlInfo), &gorm.Config{
 		SkipDefaultTransaction: true,
 	})
+	if err != nil {
+		return nil, err
+	}
+
+	rows, err := db.Raw("SELECT 1 FROM pg_database WHERE datname = ?", cfg.DBName).Rows()
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	if !rows.Next() {
+		createDBQuery := fmt.Sprintf("CREATE DATABASE %s", cfg.DBName)
+		if err := db.Exec(createDBQuery).Error; err != nil {
+			return nil, err
+		}
+		fmt.Printf("Database %s created successfully.\n", cfg.DBName)
+	}
+
+	sqlDB, err := db.DB()
+	if err != nil {
+		return nil, err
+	}
+	if err := sqlDB.Close(); err != nil {
+		return nil, err
+	}
+
+	psqlInfo = fmt.Sprintf("host=%s user=%s dbname=%s port=%s password=%s sslmode=disable",
+		cfg.DBHost, cfg.DBUser, cfg.DBName, cfg.DBPort, cfg.DBPassword)
+	db, err = gorm.Open(postgres.Open(psqlInfo), &gorm.Config{
+		SkipDefaultTransaction: true,
+	})
+	if err != nil {
+		return nil, err
+	}
 
 	db.AutoMigrate(&domain.Post{})
 	db.AutoMigrate(&domain.PostType{})
@@ -29,5 +65,6 @@ func ConnectDatabase(cfg config.Config) (*gorm.DB, error) {
 	db.AutoMigrate(&domain.SavedPost{})
 	db.AutoMigrate(&domain.PostReports{})
 	db.AutoMigrate(&domain.ViewStory{})
-	return db, dbErr
+	
+	return db, nil
 }
